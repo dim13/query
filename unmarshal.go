fd130608a271901f2812f94d6cbf360e639e0f31
Dimitri Sokolyuk <ds@doozer.de>
2016-09-14 19:40:59 +0200

    Set value

--- unmarshal.go
+++ unmarshal.go
@@ -5,15 +5,24 @@ import (
 	"log"
 	"net/url"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
+// Unmarshal url.Values to struct
 func Unmarshal(q url.Values, v interface{}) error {
 	val := reflect.ValueOf(v)
 	if val.Kind() != reflect.Ptr {
 		return errors.New("non-pointer passed to Unmarshal")
 	}
-	return unmarshal(q, val.Elem())
+	// convert url.Values keys to lowercase
+	ql := url.Values{}
+	for k, v := range q {
+		kl := strings.ToLower(k)
+		ql[kl] = append(ql[kl], v...)
+	}
+	log.Println(">>>", ql)
+	return unmarshal(ql, val.Elem())
 }
 
 func unmarshal(q url.Values, v reflect.Value) error {
@@ -24,15 +33,35 @@ func unmarshal(q url.Values, v reflect.Value) error {
 	t := v.Type()
 	for i := 0; i < t.NumField(); i++ {
 		name := strings.ToLower(t.Field(i).Name)
+		arg, ok := q[name]
+		if !ok {
+			continue
+		}
+		log.Println("arg", arg)
+		f := v.Field(i)
 		switch v.Field(i).Kind() {
 		case reflect.String:
 			log.Println("string", name)
+			f.SetString(arg[0])
 		case reflect.Int, reflect.Int32, reflect.Int64:
 			log.Println("int", name)
+			i, err := strconv.ParseInt(arg[0], 10, 64)
+			if err != nil {
+				return err
+			}
+			f.SetInt(i)
 		case reflect.Uint, reflect.Uint32, reflect.Uint64:
 			log.Println("uint", name)
+			u, err := strconv.ParseUint(arg[0], 10, 64)
+			if err != nil {
+				return err
+			}
+			f.SetUint(u)
+		case reflect.Slice:
+			log.Println("slice", name)
 		default:
 			log.Println("???", name, v.Field(i).Kind())
+			return errors.New("unsupported type")
 		}
 	}
 	return nil
--- unmarshal_test.go
+++ unmarshal_test.go
@@ -10,11 +10,16 @@ func TestUnmarshal(t *testing.T) {
 		A string
 		B int
 		C uint32
+		D []string
 	}{}
 	v := url.Values{}
 	v.Add("a", "whatever")
+	v.Add("A", "Whatever")
 	v.Add("b", "100")
 	v.Add("c", "42")
+	v.Add("d", "xxx")
+	v.Add("d", "yyy")
+	v.Add("d", "zzz")
 
 	err := Unmarshal(v, &q)
 	if err != nil {
